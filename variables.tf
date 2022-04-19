0e253ce22c670e8a35f4614934ceea1b0934c00e
chris-fison <47662472+chris-fison@users.noreply.github.com>
2022-04-19 16:43:01 +0100

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,17 @@
+# Use variables to customize the deployment
+
+variable "root_id" {
+  type    = string
+  default = "ft"
+}
+
+variable "root_name" {
+  type    = string
+  default = "fisontech-elz"
+}
+
+variable "archetype_name" {
+type = string
+default = "fisontech"
+
+}
